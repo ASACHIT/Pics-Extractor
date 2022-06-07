b736af85f3601aa5a76bdefd6ce24c2ad7a9a3bf
Shashwat Poudel <shashwatp07@gmail.com>
2022-06-07 21:41:08 +0545

    link in image, fixed styling error

--- extractor/views.py
+++ extractor/views.py
@@ -8,7 +8,7 @@ def scrape(request):
     # if form is submitted with post request then
     if request.method == "POST":
         url = request.POST.get("hero-field")  # fecthing url from input tag
-        # validating inputted url
+        # validating the input url
         if url.startswith("https") or url.startswith("http"):
             get_images = ScrapeImages(url)
             image_and_filename = get_images.get_all_images()
@@ -22,5 +22,5 @@ def scrape(request):
             messages.add_message(
                 request, messages.INFO, "Wrong Website URL, Input with Protocols"
             )
-            return redirect("extractimg")  # redirect to home page
+            return redirect("extracting")  # redirect to home page
     return render(request, "home.html")  # rendering home page
